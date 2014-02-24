b91150ed0b7e53629914bbecea014775f871698f
jeanluc.amitousa.mankoy <jeanluc.amitousa.mankoy@gmail.com>
2014-02-24 11:29:32 +0100

    Change fiel name from PeuplementJLuc to Peuplement<table_name>

---
+++ ScriptPeuplementLocation.sql
@@ -0,0 +1,12 @@
+------------------------------------------------------------
+-- Fichier     : ScriptPeuplementJeanLuc
+-- Date        : 17/02/2014
+-- Version     : 1.0
+-- Auteur      : Jean-Luc Amitousa Mankoy
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire : Remplissage de la table Location
+------------------------------------------------------------
+
+USE TAuto_IBDR
