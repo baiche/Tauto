6e9ac9fb853b2f15bb3993499b88b109aaa9e14d
Lecoconnier David <david.lecoconnier@free.fr>
2014-02-24 11:22:21 +0100

    Procedure added and directory structure defined

---
+++ Procedures/Procedure_Categorie.sql
@@ -0,0 +1,47 @@
+------------------------------------------------------------
+-- Fichier     : Procedure_Categorie
+-- Date        : 24/02/2014
+-- Version     : 1.0
+-- Auteur      : David Lecoconnier
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire :
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+GO
+CREATE PROCEDURE TAuto.createCategorie
+	@nom					nvarchar(50),
+	@description 			nvarchar(50),
+	@nom_typepermis 		nvarchar(10)
+AS
+	INSERT INTO Categorie(
+		nom, 
+		description, 
+		nom_typeperms
+	)
+	VALUES (
+		@nom,
+		@description,
+		@nom_typepermis
+	)
+	
+GO
+CREATE PROCEDURE TAuto.updateCategorie
+	@nom					nvarchar(50),
+	@description 			nvarchar(50),
+	@nom_typepermis 		nvarchar(10)
+AS
+	UPDATE Categorie
+	SET nom = @nom, description = @description, nom_typepermis = @nom_typepermis
+	WHERE nom = @nom
+	
+GO
+CREATE PROCEDURE TAuto.deleteCategorie
+	@nom					nvarchar(50),
+AS
+	DELETE FROM Categorie
+	WHERE nom = @nom
+GO
