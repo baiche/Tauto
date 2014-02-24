14fa31f00ff680d210cdde1d18ebe2c78fac771c
jeanluc.amitousa.mankoy <jeanluc.amitousa.mankoy@gmail.com>
2014-02-24 17:15:08 +0100

    change settlement nomenclature and order

--- ScriptPeuplementLocation.sql
+++ ScriptPeuplementLocation.sql
@@ -13,4 +13,22 @@ USE TAuto_IBDR
 
 --Une location en cours
 
+INSERT INTO Abonnement(date_debut,duree,renouvellement_auto,nom_typeabonnement,nom_compteabonne,
+					   prenom_compteabonne,date_naissance_compteabonne) VALUES
+('2014-02-01','60','false','5vehicules','Ouir', 'Seyyid', '1986-05-25');
+
+INSERT INTO ContratLocation(date_debut,date_fin,date_fin_effective,extension,id_abonnement) VALUES
+('2014-02-23','2014-03-23',NULL,NULL,1);
+
+INSERT INTO Location(matricule_vehicule,id_facturation,date_etat_avant,date_etat_apres,id_contratLocation) VALUES
+('0775896wx',NULL,NULL,NULL,1);
+
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+('2014-02-23',18000,'true','false','TheFicheId');
+
+UPDATE Location
+SET date_etat_avant='2014-02-23'
+WHERE id=1
+
+
 --Une location en retard
