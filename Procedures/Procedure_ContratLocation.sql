bb9a39cb7817e296125f91cb1ba8a72485e99458
Lecoconnier David <david.lecoconnier@free.fr>
2014-02-24 12:25:05 +0100

    ContratLocation procedure added

---
+++ Procedures/Procedure_ContratLocation.sql
@@ -0,0 +1,61 @@
+------------------------------------------------------------
+-- Fichier     : Procedure_ContratLocation
+-- Date        : 24/02/2014
+-- Version     : 1.0
+-- Auteur      : David Lecoconnier
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire :
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+GO
+CREATE PROCEDURE TAuto.createContratLocation
+	@date_debut 			datetime,
+	@date_fin 				datetime,
+	@date_fin_effective 	datetime,
+	@extension 				int,
+	@id_abonnement 			int
+AS
+	INSERT INTO ContratLocation (
+		date_debut,
+		date_fin,
+		date_fin_effective,
+		extension,
+		id_abonnement
+	)
+	VALUES (
+		@date_debut,
+		@date_fin,
+		@date_fin_effective,
+		@extension,
+		@id_abonnement
+	)
+
+GO
+CREATE PROCEDURE TAuto.updateContratLocation
+	@id						int,
+	@date_debut 			datetime,
+	@date_fin 				datetime,
+	@date_fin_effective 	datetime,
+	@extension 				int,
+	@id_abonnement 			int
+AS
+	UPDATE ContratLocation
+	SET
+		date_debut = @date_debut,
+		date_fin = @date_fin,
+		date_fin_effective = @date_fin_effective,
+		extension = @extension,
+		id_abonnement = @id_abonnement
+	WHERE id = @id
+
+GO
+CREATE PROCEDURE TAuto.deleteContratLocation
+	@id						int
+AS
+	DELETE FROM ContratLocation
+	WHERE id = @id
+GO
