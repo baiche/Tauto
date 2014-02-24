7b9ef061f11ff17d9d1bead9e190ec306664878b
jeanluc.amitousa.mankoy <jeanluc.amitousa.mankoy@gmail.com>
2014-02-24 14:04:57 +0100

    move ScripSuppression to Generation dir

---
+++ Generation/ScriptSuppression.sql
@@ -0,0 +1,45 @@
+------------------------------------------------------------
+-- Fichier     : ScriptSuppression
+-- Date        : 17/02/2014
+-- Version     : 1.0
+-- Auteur      : Boris de Finance
+-- Correcteur  : Seyyid Ahmed Ouir
+-- Testeur     :
+-- Integrateur : 
+-- Commentaire : Supprimer toutes les tables
+------------------------------------------------------------
+
+USE TAuto_IBDR
+
+
+DROP TABLE CatalogueCategorie;
+DROP TABLE CategorieModele;
+DROP TABLE ConducteurLocation;
+DROP TABLE CompteAbonneConducteur;
+DROP TABLE Catalogue;
+DROP TABLE Categorie;
+DROP TABLE SousPermis;
+DROP TABLE TypePermis;
+DROP TABLE Conducteur;
+DROP TABLE Permis;
+DROP TABLE Reservation;
+DROP TABLE Infraction;
+DROP TABLE Incident;
+DROP TABLE Retard;
+DROP TABLE Location;
+DROP TABLE Vehicule;
+DROP TABLE Modele;
+DROP TABLE ContratLocation;
+DROP TABLE Abonnement;
+DROP TABLE TypeAbonnement;
+DROP TABLE RelanceDecouvert;
+DROP TABLE Particulier;
+DROP TABLE Entreprise
+DROP TABLE CompteAbonne;
+DROP TABLE Facturation;
+DROP TABLE Etat;
+DROP TABLE ListeNoire;
+DROP TABLE CouleurVehicule;
+DROP TABLE Nationalite;
+DROP TABLE StatutVehicule;
+DROP TABLE TypeCarburant;
