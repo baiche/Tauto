c95a6eeb75b758b57c2938a0bfa787da10e1ec7f
sid24 <seyyid.ouir@gmail.com>
2014-02-24 14:40:07 +0100

    modif peuplement abonnement

--- ScriptPeuplementEntreprise.sql
+++ ScriptPeuplementEntreprise.sql
@@ -12,11 +12,11 @@
 USE TAuto_IBDR
 
 INSERT INTO CompteAbonne(nom,prenom,date_naissance,actif,liste_grise,iban,courriel,telephone) VALUES
-('PIndustrie', 'PIndustrie', '2014-02-17', 'true', 
+('PIndustrie', 'PIndustrie', '1980-02-17', 'true', 
  'false', 'AD12 0001 2030 2003 5910 0100', 'pindustrie@hotmail.fr', '0656470693'),
-('TASociety', 'TASociety', '2014-02-24', 'true', 
+('TASociety', 'TASociety', '1980-02-24', 'true', 
  'false', 'AL47 2121 1009 0000 0002 3569 8741', 'ta_society@hotmail.fr', '0506038405');
 
 INSERT INTO Entreprise(siret,nom,nom_compte,prenom_compte,date_naissance_compte) VALUES
-('732 829 320', 'Pokemon Industrie','PIndustrie','PIndustrie','2014-02-17'),
-('843 930 431', 'TASociety','TASociety','TASociety','2014-02-24');
+('732 829 320', 'Pokemon Industrie','PIndustrie','PIndustrie','1980-02-17'),
+('843 930 431', 'TASociety','TASociety','TASociety','1980-02-24');
