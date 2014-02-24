e31d57f10a8e514c0a41a9a051e3b7a79c9a1545
sid24 <seyyid.ouir@gmail.com>
2014-02-24 17:12:02 +0100

    Abonnement TypeAbonnement Reservation

--- ScriptPeuplementTypeAbonnement.sql
+++ ScriptPeuplementTypeAbonnement.sql
@@ -15,10 +15,10 @@ USE TAuto_IBDR
 INSERT INTO TypeAbonnement
 	(nom, prix, nb_max_vehicules)
 VALUES
-	('or', 15, 100),
-	('argent', 10, 50),
-	('bronze', 8, 20),
-	('10vehicules', 5, 10),
-	('5vehicules', 3, 5),
-	('2vehicules', 2, 2),
-	('1vehicules', 1, 1);
+	('or',          15, 100),
+	('argent',      10,  50),
+	('bronze',       8,  20),
+	('10vehicules',  5,  10),
+	('5vehicules',   3,   5),
+	('2vehicules',   2,   2),
+	('1vehicules',   1,   1);
