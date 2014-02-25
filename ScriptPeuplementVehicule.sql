dab1e2362d5d06221a40453aa953b0ff7eacd888
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-02-25 11:29:28 +0100

    Modification script peuplement vehicule

--- ScriptPeuplementVehicule.sql
+++ ScriptPeuplementVehicule.sql
@@ -13,13 +13,12 @@ USE TAuto_IBDR
 
 GO
 
-INSERT INTO Vehicule (matricule,kilometrage,couleur,etat,marque_modele,serie_modele,portieres_modele,type_carburant_modele)VALUES
-		('0775896wx','18000','Bleu','Louee','Peugeot','206',5,'Diesel'), 
-		('0775896we','14000','Bleu','Disponible','Peugeot','206',3,'Essence'),
-		('0775896wr','25000','Bleu','Disponible','Peugeot','206',5,'Diesel'),
-		('0775896wt','35000','Bleu','Disponible','Peugeot','206',5,'Diesel'), 
-		('0775896wy','30000','Bleu','Disponible','Peugeot','206',4,'Essence'), 
-		('0775896wu','60000','Bleu','Louee','Peugeot','206',5,'Diesel'), 
-		('0775896wi','120000','Bleu','Disponible','Peugeot','206',5,'Diesel')   
-		
+INSERT INTO Vehicule (matricule,kilometrage,couleur,statut,num_serie,marque_modele,serie_modele,portieres_modele,type_carburant_modele)VALUES
+		('0775896wx','18000','Bleu','En panne','VF3 8C4HXF 81100000','Peugeot','206',5,'Diesel'), 
+		('0775896we','14000','Bleu','Disponible','VF3 8C4HXF 81100000','Peugeot','206',3,'Essence'),
+		('0775896wr','25000','Bleu','Disponible','VF3 8C4HXF 81100000','Peugeot','206',5,'Diesel'),
+		('0775896wt','35000','Bleu','Disponible','VF3 8C4HXF 81100000','Peugeot','206',5,'Diesel'), 
+		('0775896wy','30000','Bleu','Disponible','VF3 8C4HXF 81100000','Peugeot','206',4,'Essence'), 
+		('0775896wu','60000','Bleu','Perdue','VF3 8C4HXF 81100000','Peugeot','206',5,'Diesel'), 
+		('0775896wi','120000','Bleu','Disponible','VF3 8C4HXF 81100000','Peugeot','206',5,'Diesel')   		
 GO
