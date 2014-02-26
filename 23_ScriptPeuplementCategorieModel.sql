8616c4d33726b887241a447e05ce59a9ef2552ba
Lecoconnier David <david.lecoconnier@free.fr>
2014-02-26 11:15:54 +0100

    CategorieModel done

--- 23_ScriptPeuplementCategorieModel.sql
+++ 23_ScriptPeuplementCategorieModel.sql
@@ -16,8 +16,19 @@ USE TAuto_IBDR
 GO
 INSERT CategorieModele(marque_modele,serie_modele,portieres_modele,nom_categorie) VALUES
 		('Peugeot','206','Essence',5,'Vehicule Simple'),
+		('Peugeot','206','Essence',3,'Vehicule Simple'),
+		('Peugeot','206','Diesel',5,'Vehicule Simple'),
 		('Peugeot','206','Diesel',3,'Vehicule Simple'),
-		('Peugeot','207','Diesel',5,'Vehicule Simple')
+		('Peugeot','207','Essence',5,'Vehicule Simple'),
+		('Peugeot','207','Diesel',3,'Vehicule Simple'),
+		('Peugeot','307','Diesel',5,'Vehicule Simple'),
+		('Peugeot','307','Essence',5,'Vehicule Simple'),
+		('Peugeot','406','Diesel',5,'Vehicule Simple'),
+		('Peugeot','406','Essence',5,'Vehicule Simple'),
+		('Peugeot','607','Diesel',5,'Vehicule Simple'),
+		('Peugeot','607','Essence',5,'Vehicule Simple'),
+		('BMW','5 F10 M5','Diesel',5'Vehicule Simple'),
+		('BMW','5 F10 M5','Essence',5'Vehicule Simple')
 GO
 
 
@@ -25,8 +36,12 @@ GO
 GO
 INSERT CategorieModele(marque_modele,serie_modele,portieres_modele,nom_categorie) VALUES
 		('Man','TGS','Essence',2,'Camion'),
+		('Man','TGS','Diesel',2,'Camion'),
+		('Toyota','DYNA 6-105 DROPSIDES','Essence',2,'Camion'),
 		('Toyota','DYNA 6-105 DROPSIDES','Diesel',2,'Camion'),
+		('Izuzu','P35.Y07','Essence',2,'Camion'),
 		('Izuzu','P35.Y07','Diesel',2,'Camion'),
+		('Mercedes-Benz','Actros','Essence',2,'Camion'),
 		('Mercedes-Benz','Actros','Diesel',2,'Camion')
 GO
 
