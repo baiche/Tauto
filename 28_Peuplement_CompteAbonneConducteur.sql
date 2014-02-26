5503aeb91dd90635a62818b05437d990d0d40363
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-02-26 18:56:31 +0100

    Version stable du script de peuplement

--- 28_Peuplement_CompteAbonneConducteur.sql
+++ 28_Peuplement_CompteAbonneConducteur.sql
@@ -13,20 +13,20 @@ USE TAuto_IBDR;
 
 GO
 INSERT INTO CompteAbonneConducteur(nom_compteabonne, prenom_compteabonne, date_naissance_compteabonne, nationalite_conducteur, piece_identite_conducteur) VALUES
-	('PIndustrie', 'PIndustrie', '2014-02-17','200000002', 'Français'),
-	('TASociety', 'TASociety', '2014-02-24','123456789', 'Français'),
-	('TASociety', 'TASociety', '2014-02-24','987654321', 'Français'),
+	('PIndustrie', 'PIndustrie', '2014-02-17','200000002', 'Francais'),
+	('TASociety', 'TASociety', '2014-02-24','123456789', 'Francais'),
+	('TASociety', 'TASociety', '2014-02-24','987654321', 'Francais'),
 	('TASociety', 'TASociety', '2014-02-24','100000001', 'Anglais')
 ;
 
 GO
 INSERT INTO CompteAbonneConducteur(nom_compteabonne, prenom_compteabonne, date_naissance_compteabonne, nationalite_conducteur, piece_identite_conducteur) VALUES
 	('Amitousa Mankoy', 'Jean-Luc', '1990-07-18', '100000001', 'Anglais'),
-	('Lecoconier', 'David', '1990-02-02', '987654321', 'Français'),
-	('De Finance', 'Boris', '1990-09-08', '123456789', 'Français'),
-	('Ouir', 'Seyyid', '1986-05-25', '200000002', 'Français'),
-	('Baiche', 'Mourad', '1989-04-25', '200000002', 'Français'),
-	('Deluze', 'Alexis', '1974-02-12', '987654321', 'Français'),
+	('Lecoconier', 'David', '1990-02-02', '987654321', 'Francais'),
+	('De Finance', 'Boris', '1990-09-08', '123456789', 'Francais'),
+	('Ouir', 'Seyyid', '1986-05-25', '200000002', 'Francais'),
+	('Baiche', 'Mourad', '1989-04-25', '200000002', 'Francais'),
+	('Deluze', 'Alexis', '1974-02-12', '987654321', 'Francais'),
 	('Mottier', 'Allan', '1989-03-23', '100000001', 'Anglais')
 ;
 
--- 29_Peuplement_ConducteurLocation.sql
+++ 29_Peuplement_ConducteurLocation.sql
@@ -15,10 +15,10 @@ USE TAuto_IBDR
 INSERT INTO ConducteurLocation
 	(id_location, piece_identite_conducteur, nationalite_conducteur)
 VALUES
-	(1, '123456789', 'Français'),
-	(1, '987654321', 'Français'),
-	(2, '987654321', 'Français'),
+	(1, '123456789', 'Francais'),
+	(1, '987654321', 'Francais'),
+	(2, '987654321', 'Francais'),
 	(3, '100000001', 'Anglais'),
-	(4, '200000002', 'Français'),
-	(5, '987654321', 'Français'),
-	(6, '123456789', 'Français');
+	(4, '200000002', 'Francais'),
+	(5, '987654321', 'Francais'),
+	(6, '123456789', 'Francais');
