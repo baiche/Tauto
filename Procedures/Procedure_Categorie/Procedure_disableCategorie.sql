ff664586e01598b6368e109dce550378d106dc74
baiche <baiche.mourad89@live.fr>
2014-03-04 16:38:51 +0100

    ajout des procedures pour la table CategorieModele ( Baiche )

---
+++ Procedures/Procedure_Categorie/Procedure_disableCategorie.sql
@@ -0,0 +1,22 @@
+------------------------------------------------------------
+-- Fichier     : Procedure_disableCategorie
+-- Date        : 04/03/2014
+-- Version     : 1.0
+-- Auteur      : Baiche Mourad
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire :
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+GO
+CREATE PROCEDURE dbo.disableCategorie
+	@nom					nvarchar(50)
+AS
+	UPDATE Categorie
+	SET a_spprimer='true'; 
+	WHERE nom = @nom;
+	
+GO
