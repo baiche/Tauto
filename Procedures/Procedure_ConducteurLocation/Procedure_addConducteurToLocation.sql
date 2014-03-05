b49e23dd6bca2175774e9b8c176f2a4aba9043d1
Lecoconnier David <david.lecoconnier@free.fr>
2014-03-05 11:57:00 +0100

    ConducteurLocation procedure created

---
+++ Procedures/Procedure_ConducteurLocation/Procedure_addConducteurToLocation.sql
@@ -0,0 +1,50 @@
+------------------------------------------------------------
+-- Fichier     : Procedure_addConducteurToLocation
+-- Date        : 05/03/2014
+-- Version     : 1.0
+-- Auteur      : David Lecoconnier
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire : Crée une jointure seulement si le tuple n'existe pas.
+--				 Renvoie 1 en cas de succès, -1 autrement
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+IF OBJECT_ID ('dbo.addConducteurToLocation', 'P') IS NOT NULL
+	DROP PROCEDURE dbo.addConducteurToLocation
+
+GO
+CREATE PROCEDURE dbo.addConducteurToLocation
+AS
+	DECLARE	@id_location 						int,
+	DECLARE	@piece_identite_conducteur 			nvarchar(50),
+	DECLARE	@nationalite_conducteur 			nvarchar(50)
+BEGIN
+	IF ( (SELECT COUNT (*) FROM ConducteurLocation WHERE
+		id_location = @id_location AND
+		piece_identite_conducteur = @piece_identite_conducteur AND
+		nationalite_conducteur = @nationalite_conducteur
+	) = 0)
+	BEGIN
+		INSERT INTO ConducteurLocation (
+			id_location,
+			piece_identite_conducteur,
+			nationalite_conducteur
+		)
+		VALUES (
+			@id_location,
+			@piece_identite_conducteur,
+			@nationalite_conducteur
+		);
+		PRINT('Conducteur ajouté à la location');
+		RETURN 1;
+	END
+	ELSE
+	BEGIN
+		PRINT('addConducteurToLocation: ERROR, tuple existant');
+		RETURN -1;
+	END
+END
+GO
