3fee7d129d1c5c5c0175e91884fa36eb0a40e454
jeanluc.amitousa.mankoy <jeanluc.amitousa.mankoy@gmail.com>
2014-03-06 23:22:06 +0100

    finish implementing tests for Etat table

--- Tests/20140306_SACT_Etat.sql
+++ Tests/20140306_SACT_Etat.sql
@@ -43,18 +43,274 @@ INSERT INTO Location(matricule_vehicule,id_facturation,date_etat_avant,date_etat
 		 (SELECT id FROM ContratLocation WHERE date_debut='2060-12-02 00:00:00')); 		
 
 --Test A.1
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'0100');
+	PRINT('------------------------------Test A.1 OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test A.1 NOT OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+
 --Test A.2
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 -1,0,'0100');
+	PRINT('------------------------------Test A.2 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test A.2 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test A.3
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 NULL,0,'0100');
+	PRINT('------------------------------Test A.3 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test A.3 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test A.4
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 0,'0100');
+	IF (SELECT km FROM Etat WHERE date_creation='25-11-2060' 
+							  AND id_location=(SELECT id 
+											   FROM Location 
+											   WHERE matricule_vehicule='1885896wx')) = 0
+		PRINT('------------------------------Test A.4 OK');
+	ELSE
+		PRINT('------------------------------Test A.4 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test A.4 NOT OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test B.1
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,'0100');
+	PRINT('------------------------------Test B.1 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test B.1 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test B.2
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,NULL,'0100');
+	PRINT('------------------------------Test B.2 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test B.2 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test C.1
+BEGIN TRY
+INSERT INTO Etat(id_location,km,degat,fiche) VALUES
+			((SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'0100');
+	IF (SELECT km FROM Etat WHERE date_creation='25-11-2060' 
+							  AND id_location=(SELECT id 
+											   FROM Location 
+											   WHERE matricule_vehicule='1885896wx')) = NULL
+		PRINT('------------------------------Test C.1 NOT OK');
+	ELSE
+		PRINT('------------------------------Test C.1 OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.1 NOT OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test C.2
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			(NULL, 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'0100');
+	PRINT('------------------------------Test C.2 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.2 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test D.1
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 -11,
+			 10,0,'0100');
+	PRINT('------------------------------Test D.1 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test D.1 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test D.2
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060',NULL,10,0,'0100');
+	PRINT('------------------------------Test D.2 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test D.2 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+--Test D.3
+BEGIN TRY
+INSERT INTO Etat(date_creation,km,degat,fiche) VALUES
+			('25-11-2060',10,0,'0100');
+	PRINT('------------------------------Test D.3 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test D.3 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test E.1
+----------Part 1
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'');
+	PRINT('------------------------------Test E.1 NOT OK.1');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test E.1 OK.1');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+
+----------Part 2		  
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,' ');
+	PRINT('------------------------------Test E.1 NOT OK.2');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test E.1 OK.2');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+
+----------Part 3
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'m@Fiche');
+	PRINT('------------------------------Test E.1 NOT OK.3');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test E.1 OK.3');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
 --Test E.2
 
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,NULL);
+	PRINT('------------------------------Test E.2 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test E.2 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+
+
+--Test F.1
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'0100'),
+			 ('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 11,1,'0200');
+	PRINT('------------------------------Test E.1 NOT OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test E.1 OK');
+END CATCH
+
+DELETE FROM Etat WHERE date_creation='25-11-2060' 
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
+				  
+--Test F.2
+BEGIN TRY
+INSERT INTO Etat(date_creation,id_location,km,degat,fiche) VALUES
+			('25-11-2060', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 10,0,'0100'),
+			 ('25-11-2061', 
+			 (SELECT id FROM Location WHERE matricule_vehicule='1885896wx'),
+			 11,1,'0200');
+	PRINT('------------------------------Test F.2 OK');
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test F.2 NOT OK');
+END CATCH
+
+DELETE FROM Etat WHERE (date_creation='25-11-2060' OR date_creation='25-11-2061')
+				  AND id_location=(SELECT id FROM Location WHERE matricule_vehicule='1885896wx');
 
 ------------------------------
 --CLEAN FOR ALL TESTS
