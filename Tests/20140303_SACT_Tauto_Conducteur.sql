a0a6c0507807147d4a0cf4a8d5ee25ced5ec20de
Alexis <Alexis@salleTA>
2014-03-06 20:16:28 +0100

    Modif test Conducteur

--- Tests/20140303_SACT_Tauto_Conducteur.sql
+++ Tests/20140303_SACT_Tauto_Conducteur.sql
@@ -114,62 +114,6 @@ BEGIN CATCH
 END CATCH 
 DELETE FROM Conducteur;
 
---Test A8
-
-BEGIN TRY
-	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
-		('', 'Francais', 'Deluze', 'Alexis', '0000000001');
-    
-    PRINT('------------------------------Test A.8 NOT OK')
-		
-END TRY
-BEGIN CATCH
-	PRINT('------------------------------Test A.8 OK')
-END CATCH 
-DELETE FROM Conducteur;
-
---Test A9
-
-BEGIN TRY
-	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
-		('123456789', '', 'Deluze', 'Alexis', '0000000001');
-    
-    PRINT('------------------------------Test A.9 NOT OK')
-		
-END TRY
-BEGIN CATCH
-	PRINT('------------------------------Test A.9 OK')
-END CATCH 
-DELETE FROM Conducteur;
-
---Test A10
-
-BEGIN TRY
-	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
-		('123456789', 'Francais', '', 'Alexis', '0000000001');
-    
-    PRINT('------------------------------Test A.10 NOT OK')
-		
-END TRY
-BEGIN CATCH
-	PRINT('------------------------------Test A.10 OK')
-END CATCH 
-DELETE FROM Conducteur;
-
---Test A11
-
-BEGIN TRY
-	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
-		('123456789', 'Francais', 'Deluze', 'Alexis', '');
-    
-    PRINT('------------------------------Test A.11 NOT OK')
-		
-END TRY
-BEGIN CATCH
-	PRINT('------------------------------Test A.11 OK')
-END CATCH 
-DELETE FROM Conducteur;
-
 --Test B1
 
 BEGIN TRY
@@ -204,17 +148,90 @@ BEGIN CATCH
 END CATCH 
 DELETE FROM Conducteur;
 
+--Test B3
+
+BEGIN TRY
+	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', 'Francais', 'Deluze', 'Alexis', '0000000001');
+    
+    INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', 'Anglais', 'Deluze', 'Alexis', '0000000001');
+	
+	PRINT('------------------------------Test B.3 OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test B.3 NOT OK')
+END CATCH 
+DELETE FROM Conducteur;
+
 --Test C1
 
 BEGIN TRY
 	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
-		('', '0000000001', '2001-01-01', '9999-12-31', DEFAULT);
-    	
-	PRINT('------------------------------Test C.1 NOT OK')
+		('', 'Francais', 'Deluze', 'Alexis', '0000000001');
+    
+    PRINT('------------------------------Test C.1 NOT OK')
 		
 END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test C.1 OK')
+END CATCH 
+DELETE FROM Conducteur;
+
+--Test C2
+
+BEGIN TRY
+	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', '', 'Deluze', 'Alexis', '0000000001');
+    
+    PRINT('------------------------------Test C.2 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.2 OK')
+END CATCH 
+DELETE FROM Conducteur;
+
+--Test C3
+
+BEGIN TRY
+	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', 'Francais', '', 'Alexis', '0000000001');
+    
+    PRINT('------------------------------Test C.3 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.3 OK')
+END CATCH 
+DELETE FROM Conducteur;
+
+--Test C4
+
+BEGIN TRY
+	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', 'Francais', 'Deluze', '', '0000000001');
+    
+    PRINT('------------------------------Test C.4 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.4 OK')
+END CATCH 
+DELETE FROM Conducteur;
+
+--Test C5
+
+BEGIN TRY
+	INSERT INTO Conducteur(piece_identite, nationalite, nom, prenom, id_permis) VALUES
+		('123456789', 'Francais', 'Deluze', 'Alexis', '');
+    
+    PRINT('------------------------------Test C.5 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.5 OK')
 END CATCH 
 DELETE FROM Conducteur;
 
