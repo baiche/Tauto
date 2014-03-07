f879562a7f804dae545fd35daaab8a1a54ea6699
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-03-07 09:44:35 +0100

    Correction createListeNoire

--- Procedures/Procedure_ListeNoire/Procedure_createListeNoire.sql
+++ Procedures/Procedure_ListeNoire/Procedure_createListeNoire.sql
@@ -3,7 +3,7 @@
 -- Date        : 24/02/2014
 -- Version     : 1.0
 -- Auteur      : Mourad Baiche
--- Correcteur  : 
+-- Correcteur  : Boris de Finance
 -- Testeur     : 
 -- Integrateur : 
 -- Commentaire :
@@ -11,8 +11,11 @@
 
 USE TAuto_IBDR;
 
+IF OBJECT_ID ('dbo.createListeNoire', 'P') IS NOT NULL
+DROP PROCEDURE dbo.createListeNoire;
 GO
-CREATE PROCEDURE TAuto.createListeNoire
+
+CREATE PROCEDURE dbo.createListeNoire
 	@date_naissance	 		date,
 	@nom					nvarchar(50),
 	@prenom					nvarchar(50)
