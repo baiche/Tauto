bfe47488089eaaaf28c144eaf68e171210f6b860
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-03-08 12:50:55 +0100

    affichage de la facture d'un contrat location première version finie.

--- Procedures/Procedure_ContratLocation/Procedure_printContratLocation.sql
+++ Procedures/Procedure_ContratLocation/Procedure_printContratLocation.sql
@@ -24,6 +24,9 @@ AS
 		SELECT id FROM Location WHERE Location.id_contratLocation = @id_contrat_location
 		DECLARE @nb_location int
 		DECLARE @id_location int
+		DECLARE @total money
+		SET @total = 0
+		DECLARE @total_tva money
 		SET @nb_location = (SELECT COUNT(*) FROM #Temp)
 		PRINT 'contrat_location numero : ' + CONVERT(varchar(10), @id_contrat_location) + ' nombre de véhicules loué(s) : ' + CONVERT(varchar(10), @nb_location)
 		DECLARE Curseur_location CURSOR LOCAL FOR SELECT id_location FROM dbo.#Temp; 
@@ -32,9 +35,16 @@ AS
 		WHILE	@@FETCH_STATUS=0
 		BEGIN	
 			EXEC dbo.printFacturation @id_location;
+			SET @total += (SELECT  f.montant 
+							FROM Facturation f, Location l
+							WHERE l.id = @id_location
+							AND l.id_facturation = f.id)
+							
 			FETCH NEXT FROM Curseur_location INTO @id_location
 		END
 		CLOSE Curseur_Location
+		SET @total_tva = @total - (@total / 1.196)
+		PRINT 'total : ' + CONVERT(varchar(10),@total) + ' dont TVA : ' + CONVERT(varchar(10), @total_tva)
 	END TRY
 	BEGIN CATCH
 	END CATCH	
