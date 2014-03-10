88e3041788d9a8594278e13c6bbf05d79ddc58d7
Lecoconnier David <david.lecoconnier@free.fr>
2014-03-10 20:17:43 +0100

    createContratLocation updated

--- Procedures/Procedure_ContratLocation/Procedure_createContratLocation.sql
+++ Procedures/Procedure_ContratLocation/Procedure_createContratLocation.sql
@@ -6,7 +6,8 @@
 -- Correcteur  : 
 -- Testeur     : 
 -- Integrateur : 
--- Commentaire : Crée un contrat de location et renvoie l'id de ce contrat
+-- Commentaire : Crée un contrat de location et renvoie l'id de ce contrat, -1 en cas d'erreur
+--				 Vérifie que le contrat puisse être créé pendant la période de validité de l'abonnement
 ------------------------------------------------------------
 
 USE TAuto_IBDR;
@@ -21,6 +22,46 @@ CREATE PROCEDURE dbo.createContratLocation
 	@id_abonnement 			int
 AS
 	BEGIN TRY
+		CREATE TABLE #TEMP (
+			date_debut 					datetime,
+			duree						int,
+			renouvellement_auto			bit
+		)
+		DECLARE @date_debut_curseur datetime, @duree_curseur int, @renouvellement_auto_curseur bit;
+		
+		SELECT date_debut, duree, renouvellement_auto_curseur
+		INTO #TEMP
+		FROM Abonnement 
+		WHERE id = @id_abonnement;
+		
+		IF ( (SELECT COUNT(*) FROM #TEMP) = 0 )
+		BEGIN
+			Print('createContratLocation: aucun abonnement correspondant');
+			RETURN -1;
+		END
+			
+		DECLARE abonne_cursor CURSOR
+			FOR SELECT * FROM #TEMP
+			
+		OPEN abonne_cursor
+		FETCH NEXT FROM abonne_cursor
+			INTO @date_debut_curseur, @duree_curseur, @renouvellement_auto_curseur;
+		CLOSE abonne_cursor;
+		DEALLOCATE abonne_cursor;
+		
+		IF ( @date_debut_curseur > @date_debut )
+		BEGIN
+			Print('createContratLocation: Date de début inférieur à celle de l''abonnement');
+			RETURN -1;
+		END
+		
+		
+		IF ( DATEADD(day, @duree_curseur, @date_debut_curseur) > @date_fin && @renouvellement_auto_curseur = 'false')
+		BEGIN
+			Print('createContratLocation: Date de fin supérieure à celle de l''abonnement');
+			RETURN -1;
+		END
+		
 		INSERT INTO ContratLocation (
 			date_debut,
 			date_fin,
---
+++ TPS/ContratLocation/20140310_TPS_TAuto_createContratLocation.sql
@@ -0,0 +1,148 @@
+------------------------------------------------------------
+-- Fichier     : 20140310_TPS_TAuto_createContratLocation
+-- Date        : 10/03/2014
+-- Version     : 1.0
+-- Auteur      : David Lecoconnier
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire : Test de la procédure de création de contrat de location
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+--Test 1
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.createContratLocation 
+			@date_debut 			datetime,
+			@date_fin 				datetime,
+			@id_abonnement 			int
+	IF ( @ReturnValue = SELECT COUNT(*) FROM ContratLocation)
+	BEGIN
+		PRINT('------------------------------Test 1 - Tuple inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 1 - Tuple non inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ContratLocation WHERE
+			date_debut = '2014-02-21' AND
+			date_fin = '2014-02-25' AND
+			id_abonnement = 11
+		) = 1)
+	BEGIN
+		PRINT('------------------------------Test 1 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 1 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 1 NOT -- OK');
+END CATCH
+GO
+
+--Test 2
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.createContratLocation 
+			@date_debut 			datetime,
+			@date_fin 				datetime,
+			@id_abonnement 			int
+	IF ( @ReturnValue = -1)
+	BEGIN
+		PRINT('------------------------------Test 2 - Tuple non inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 2 - Tuple inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ContratLocation WHERE
+			date_debut = '2014-02-21' AND
+			date_fin = '2014-02-25' AND
+			id_abonnement = 25
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 2 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 2 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 2 NOT -- OK');
+END CATCH
+GO
+
+--Test 3
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.createContratLocation 
+			@date_debut 			datetime,
+			@date_fin 				datetime,
+			@id_abonnement 			int
+	IF ( @ReturnValue = -1)
+	BEGIN
+		PRINT('------------------------------Test 3 - Tuple non inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 3 - Tuple inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ContratLocation WHERE
+			date_debut = '2014-02-25' AND
+			date_fin = '2014-03-26' AND
+			id_abonnement = 11
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 3 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 3 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 3 NOT -- OK');
+END CATCH
+GO
+
+--Test 4
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.createContratLocation 
+			@date_debut 			datetime,
+			@date_fin 				datetime,
+			@id_abonnement 			int
+	IF ( @ReturnValue = -1)
+	BEGIN
+		PRINT('------------------------------Test 4 - Tuple non inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 4 - Tuple inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ContratLocation WHERE
+			date_debut = '2014-02-17' AND
+			date_fin = '2014-02-25' AND
+			id_abonnement = 11
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 4 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 4 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 4 NOT -- OK');
+END CATCH
+GO
