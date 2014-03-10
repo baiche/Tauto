0bd5fdbf45f51bdd34868b9b759c1ea00a323d3c
Lecoconnier David <david.lecoconnier@free.fr>
2014-03-10 14:05:24 +0100

    addConducteurToLocation tested

--- TPS/ConducteurLocation/20140310_TPS_TAuto_addConducteurToLocation.sql
+++ TPS/ConducteurLocation/20140310_TPS_TAuto_addConducteurToLocation.sql
@@ -44,3 +44,105 @@ BEGIN CATCH
 	PRINT('------------------------------Test 1 NOT -- OK');
 END CATCH
 GO
+
+--Test 2
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.addConducteurToLocation 
+			@id_location = 10,
+			@piece_identite_conducteur = 987654321,
+			@nationalite_conducteur = 'Francais'
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 2 - Tuple inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 2 - Tuple non inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ConducteurLocation WHERE
+			id_location = 10 AND
+			piece_identite_conducteur = 987654321 AND
+			nationalite_conducteur = 'Francais'
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 2 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 2 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 2 NOT -- OK');
+END CATCH
+GO
+
+--Test 3
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.addConducteurToLocation 
+			@id_location = 6,
+			@piece_identite_conducteur = 987622222,
+			@nationalite_conducteur = 'Francais'
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 3 - Tuple inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 3 - Tuple non inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ConducteurLocation WHERE
+			id_location = 6 AND
+			piece_identite_conducteur = 987622222 AND
+			nationalite_conducteur = 'Francais'
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 3 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 3 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 3 NOT -- OK');
+END CATCH
+GO
+
+--Test 4
+BEGIN TRY
+	DECLARE @ReturnValue int
+	EXEC @ReturnValue = dbo.addConducteurToLocation 
+			@id_location = 6,
+			@piece_identite_conducteur = 987654321,
+			@nationalite_conducteur = 'Anglais'
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 4 - Tuple inséré');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 4 - Tuple non inséré');
+	END
+	
+	IF (  (SELECT COUNT (*) FROM ConducteurLocation WHERE
+			id_location = 6 AND
+			piece_identite_conducteur = 987654321 AND
+			nationalite_conducteur = 'Anglais'
+		) = 0)
+	BEGIN
+		PRINT('------------------------------Test 4 OK');
+	END
+	ELSE
+	BEGIN
+		PRINT('------------------------------Test 4 NOT -- OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 4 NOT -- OK');
+END CATCH
+GO
