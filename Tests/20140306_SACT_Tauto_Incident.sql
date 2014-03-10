9594347a03f27c0a1772230a3c1ed802b53b6517
Diatirios <earendel.AM@gmail.com>
2014-03-10 16:26:49 +0100

    abc

--- Tests/20140306_SACT_Tauto_Incident.sql
+++ Tests/20140306_SACT_Tauto_Incident.sql
@@ -1,15 +1,15 @@
 ------------------------------------------------------------
--- Fichier     : 20140303_SACT_Tauto_Retard.sql
--- Date        : 05/03/2014
+-- Fichier     : 20140306_SACT_Tauto_Incident.sql
+-- Date        : 06/03/2014
 -- Version     : 1.0
 -- Auteur      : Allan Mottier
 -- Correcteur  : 
 -- Testeur     : 
 -- Integrateur : 
--- Commentaire : Test des contraintes sur la table Retard
+-- Commentaire : Test des contraintes sur la table Incident
 ------------------------------------------------------------
 
-USE TAuto_IBDR;
+USE Tauto_IBDR;
 
 INSERT INTO Location(matricule_vehicule,id_facturation,date_etat_avant,date_etat_apres,id_contratLocation) VALUES
 		(NULL,NULL,NULL,NULL,NULL);
@@ -18,12 +18,12 @@ INSERT INTO Location(matricule_vehicule,id_facturation,date_etat_avant,date_etat
 --Test A1
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
+	INSERT INTO Incident(id_location) VALUES
 		(1);
     
-	IF(SELECT COUNT(*) FROM Retard
+	IF(SELECT COUNT(*) FROM Incident
 		WHERE id_location = 1
-			AND DATEDIFF(minute,date,GETDATE()) < 2) = 1
+			AND DATEDIFF(minute,date,GETDATE()) < 2 ) = 1
 		PRINT('------------------------------Test A.1 OK')
 	ELSE
 		PRINT('------------------------------Test A.1 NOT OK')
@@ -32,14 +32,14 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test A.1 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test A2
 
 BEGIN TRY
-	INSERT INTO Retard(date,id_location) VALUES
+	INSERT INTO Incident(date,id_location) VALUES
 		('2014-03-06',1);
-    IF(SELECT date FROM Retard
+    IF(SELECT date FROM Incident
 		WHERE id_location = 1) = '2014-03-06'
 		PRINT('------------------------------Test A.2 OK')
 	ELSE
@@ -49,12 +49,12 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test A.2 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test A3
 
 BEGIN TRY
-	INSERT INTO Retard(date,id_location) VALUES
+	INSERT INTO Incident(date,id_location) VALUES
 		(NULL,1);
      
 	PRINT('------------------------------Test A.3 NOT OK')
@@ -63,12 +63,12 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test A.3 OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test B1
 
 BEGIN TRY
-	INSERT INTO Retard(date) VALUES ('2014-03-06');
+	INSERT INTO Incident(date) VALUES ('2014-03-06');
      
 	PRINT('------------------------------Test B.1 NOT OK')
 		
@@ -76,31 +76,31 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test B.1 OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test B2
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
+	INSERT INTO Incident(id_location) VALUES
 		(1);
     
-	IF(SELECT COUNT(*) FROM Retard
+	IF(SELECT COUNT(*) FROM Incident
 		WHERE id_location = 1
-			AND DATEDIFF(minute,date,GETDATE()) <2 ) = 1
+			AND DATEDIFF(minute,date,GETDATE()) < 2 ) = 1
 		PRINT('------------------------------Test B.2 OK')
 	ELSE
 		PRINT('------------------------------Test B.2 NOT OK')
 		
 END TRY
 BEGIN CATCH
-	PRINT('------------------------------Test B.2 NOT OK2')
+	PRINT('------------------------------Test B.2 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test B3
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
+	INSERT INTO Incident(id_location) VALUES
 		(NULL);
     
 	PRINT('------------------------------Test B.3 NOT OK')
@@ -109,16 +109,16 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test B.3 OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test C1
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
-		(1);
+	INSERT INTO Incident(id_location,description) VALUES
+		(1,'description valide');
     
-	IF(SELECT regle FROM Retard
-		WHERE id_location = 1) = 'false'
+	IF(SELECT description FROM Incident
+		WHERE id_location = 1) = 'description valide'
 		PRINT('------------------------------Test C.1 OK')
 	ELSE
 		PRINT('------------------------------Test C.1 NOT OK')
@@ -127,34 +127,62 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test C.1 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test C2
 
 BEGIN TRY
-	INSERT INTO Retard(id_location,regle) VALUES
-		(1,'true');
+	INSERT INTO Incident(id_location,description) VALUES
+		(1,'@description');
     
-	IF(SELECT regle FROM Retard
-		WHERE id_location = 1) = 'true'
-		PRINT('------------------------------Test C.2 OK')
-	ELSE
-		PRINT('------------------------------Test C.2 NOT OK')
+	PRINT('------------------------------Test C.2 NOT OK')
 		
 END TRY
 BEGIN CATCH
-	PRINT('------------------------------Test C.2 NOT OK')
+	PRINT('------------------------------Test C.2 OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
+
+--Test C3
+
+BEGIN TRY
+	INSERT INTO Incident(id_location) VALUES
+		(1);
+    
+	IF(SELECT description FROM Incident
+		WHERE id_location = 1) = ''
+		PRINT('------------------------------Test C.3 OK')
+	ELSE
+		PRINT('------------------------------Test C.3 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.3 NOT OK')
+END CATCH 
+DELETE FROM Incident;
+
+--Test C4
+
+BEGIN TRY
+	INSERT INTO Incident(id_location,description) VALUES
+		(1,NULL);
+
+		PRINT('------------------------------Test C.4 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test C.4 OK')
+END CATCH 
+DELETE FROM Incident;
 
 --Test D1
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
+	INSERT INTO Incident(id_location) VALUES
 		(1);
     
-	IF(SELECT niveau FROM Retard
-		WHERE id_location = 1) = 1
+	IF(SELECT penalisable FROM Incident
+		WHERE id_location = 1) = 'false'
 		PRINT('------------------------------Test D.1 OK')
 	ELSE
 		PRINT('------------------------------Test D.1 NOT OK')
@@ -163,16 +191,16 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test D.1 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 --Test D2
 
 BEGIN TRY
-	INSERT INTO Retard(id_location,niveau) VALUES
-		(1,2);
+	INSERT INTO Incident(id_location,penalisable) VALUES
+		(1,'true');
     
-	IF(SELECT niveau FROM Retard
-		WHERE id_location = 1) = 2
+	IF(SELECT penalisable FROM Incident
+		WHERE id_location = 1) = 'true'
 		PRINT('------------------------------Test D.2 OK')
 	ELSE
 		PRINT('------------------------------Test D.2 NOT OK')
@@ -181,12 +209,26 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test D.2 NOT OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
+
+--Test D3
+
+BEGIN TRY
+	INSERT INTO Incident(id_location,penalisable) VALUES
+		(1,NULL);
+    
+		PRINT('------------------------------Test D.3 NOT OK')
+		
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test D.3 OK')
+END CATCH 
+DELETE FROM Incident;
 
 --Test E1
 
 BEGIN TRY
-	INSERT INTO Retard(id_location) VALUES
+	INSERT INTO Incident(id_location) VALUES
 		(1),
 		(1);
     
@@ -196,6 +238,6 @@ END TRY
 BEGIN CATCH
 	PRINT('------------------------------Test E.1 OK')
 END CATCH 
-DELETE FROM Retard;
+DELETE FROM Incident;
 
 DELETE FROM Location;
