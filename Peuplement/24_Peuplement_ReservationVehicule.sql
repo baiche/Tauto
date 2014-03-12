2facff31555018a24121a797c094989db44ee94e
sid24 <seyyid.ouir@gmail.com>
2014-03-12 22:10:54 +0100

    peuplement, reservation, modif

--- Peuplement/24_Peuplement_ReservationVehicule.sql
+++ Peuplement/24_Peuplement_ReservationVehicule.sql
@@ -5,29 +5,128 @@
 -- Auteur      : Boris de Finance
 -- Correcteur  : 
 -- Testeur     : 
--- Integrateur : Boris de Finance
+-- Integrateur : Boris de Finance, Seyyid Ouir
 -- Commentaire : Liaison des reservations aux vehicules 
 -- concernés
                  
 ------------------------------------------------------------
 
 USE TAuto_IBDR;
+GO
+
+DECLARE @idReservation int;
+
+-- reservations annulees -----------------------------------
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20130502 08:00:00' AND date_fin = '20130531 18:00:00';
 
 INSERT INTO ReservationVehicule
 	(id_reservation,matricule_vehicule)
 VALUES
-(1,'0775896wu'),
-(2,'0775896wy'),
-(3,'0775896wi'),
-(4,'0775896wu'),
-(5,'0775896wy'),
-(6,'0775896wt'),
-(7,'0775896wr'),
-(8,'0775896wx'),
-(9,'0775896we'),
-(10,'0775896wr'),
-(11,'0775896wt'),
-(12,'0775896wx'),
-(13,'0775896we'),
-(14,'0775896wt'),
-(15, '0775896wi');
+	(@idReservation, '0775896wi'), -- '206',5,'Diesel'
+	(@idReservation, '0775896wu'), -- '206',5,'Diesel'
+	(@idReservation, '0775896wy'); -- '206',3,'Essence'
+	
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20130615 10:00:00' AND date_fin = '20130625 18:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wt'); -- '206',5,'Diesel'
+	
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20130701 09:00:00' AND date_fin = '20130715 17:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wr'); -- '406',5,'Diesel'
+	
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140114 08:00:00' AND date_fin = '20140121 08:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wu'); -- '206',5,'Diesel'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140121 08:00:00' AND date_fin = '20140123 12:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wy'); -- '206',3,'Essence'
+	
+
+--------------------
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140325 08:00:00' AND date_fin = '20140404 17:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896we'); -- '406',5,'Essence'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140406 13:00:00' AND date_fin = '20140410 18:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wt'), -- '206',5,'Diesel'
+	(@idReservation, '0775896wi'); -- '206',5,'Diesel'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140304 10:00:00' AND date_fin = '20140324 18:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896we'), -- '406',5,'Essence'
+	(@idReservation, '0775896wr'); -- '406',5,'Diesel'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140506 10:00:00' AND date_fin = '20140508 18:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wt'); -- '206',5,'Diesel'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140601 09:00:00' AND date_fin = '20140613 17:00:00'; -- annulee
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wy'); -- '206',3,'Essence'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20140711 09:00:00' AND date_fin = '20140922 17:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wi'); -- '206',5,'Diesel'
+
+
+SELECT @idReservation = id FROM Reservation
+WHERE date_debut = '20141105 08:00:00' AND date_fin = '20141105 16:00:00';
+
+INSERT INTO ReservationVehicule
+	(id_reservation,matricule_vehicule)
+VALUES
+	(@idReservation, '0775896wr'); -- '406',5,'Diesel'
