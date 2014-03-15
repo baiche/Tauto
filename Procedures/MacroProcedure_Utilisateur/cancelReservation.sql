95a9dad5a6c05a10c9f50e371829b35234197a28
Lecoconnier David <david.lecoconnier@free.fr>
2014-03-15 13:54:45 +0100

    Some macro procedures added

---
+++ Procedures/MacroProcedure_Utilisateur/cancelReservation.sql
@@ -0,0 +1,32 @@
+------------------------------------------------------------
+-- Fichier     : cancelReservation.sql
+-- Date        : 15/03/2014
+-- Version     : 1.0
+-- Auteur      : 
+-- Correcteur  : 
+-- Testeur     : 
+-- Integrateur : 
+-- Commentaire : 
+------------------------------------------------------------
+
+USE TAuto_IBDR;
+
+IF OBJECT_ID ('dbo.cancelReservation', 'P') IS NOT NULL
+	DROP PROCEDURE dbo.cancelReservation	
+GO
+
+CREATE PROCEDURE dbo.cancelReservation
+	@id_reservation 		int
+AS
+	BEGIN TRANSACTION cancelReservation
+	BEGIN TRY
+		COMMIT TRANSACTION cancelReservation
+		PRINT('cancelReservation OK');
+		RETURN 1;
+	END TRY
+	BEGIN CATCH
+		PRINT('cancelReservation: ERROR');
+		ROLLBACK TRANSACTION cancelReservation
+		RETURN -1;
+	END CATCH
+GO
