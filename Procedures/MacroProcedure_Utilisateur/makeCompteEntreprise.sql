3ccc3633523a70049ad1d4f16b60e606ddfd7431
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-03-16 18:41:48 +0100

    Macro procedures

--- Procedures/MacroProcedure_Utilisateur/makeCompteEntreprise.sql
+++ Procedures/MacroProcedure_Utilisateur/makeCompteEntreprise.sql
@@ -26,6 +26,77 @@ CREATE PROCEDURE dbo.makeCompteEntreprise
 	@nom_entreprise		nvarchar(50)
 AS
 	BEGIN TRANSACTION makeCompteEntreprise
+	
+	--On veut s'assurer que l'on peut ajouter le CompteAbonne
+	
+	--Gestion de la liste noire
+	DECLARE @isInListeNoire		INT
+	BEGIN TRY
+		EXEC @isInListeNoire = dbo.isInListeNoire @nom,@prenom,@date_naissance;
+
+		IF(@isInListeNoire = 1)
+		BEGIN
+			PRINT('makeCompteEntreprise: La personne est sur liste noire');
+			ROLLBACK TRANSACTION makeCompteEntreprise
+			RETURN -1
+		END
+	END TRY
+	BEGIN CATCH
+		PRINT('makeCompteEntreprise: ERROR');
+		ROLLBACK TRANSACTION makeCompteEntreprise
+			RETURN -1
+	END CATCH
+	
+	--Si la personne n'existe pas déjà
+	
+	IF((
+		SELECT COUNT(*) 
+		FROM CompteAbonne
+		WHERE nom = @nom
+		AND prenom = @prenom
+		AND date_naissance = @date_naissance) = 0)
+		BEGIN 
+			--ajout du compte abonne
+			EXEC dbo.createCompteAbonne @nom, @prenom, @date_naissance, 1, 0, @iban,@telephone, @courriel;
+			 
+			--ajout dans la table particulier
+			INSERT INTO Entreprise(nom_compte, prenom_compte, date_naissance_compte, siret, nom)
+			VALUES (@nom,@prenom,@date_naissance, @siret,@nom_entreprise)
+			COMMIT TRANSACTION makeCompteEntreprise
+			PRINT('makeCompteEntreprise OK');
+			RETURN 1;
+		END
+	ELSE
+	-- Si la personne existe déjà
+		--On regarde si le compte est actif
+		IF((
+			SELECT actif 
+			FROM CompteAbonne
+			WHERE nom = @nom
+			AND prenom = @prenom
+			AND date_naissance = @date_naissance) =  1
+		)
+		-- s'il l'est on le signale
+		BEGIN 
+			PRINT 'makeCompteEntreprise : Le compte existe déjà'
+			ROLLBACK TRANSACTION makeCompteEntreprise
+			RETURN -1;
+		END
+		ELSE
+		-- s'il ne l'est pas on le rend actif
+		BEGIN
+			UPDATE CompteAbonne
+			SET actif = 1
+			WHERE nom = @nom
+			AND prenom = @prenom
+			AND date_naissance = @date_naissance
+			COMMIT TRANSACTION makeCompteEntreprise
+			PRINT('makeCompteEntreprise OK');
+			RETURN 1 	
+		END	
+	
+	
+	
 	BEGIN TRY
 		COMMIT TRANSACTION makeCompteEntreprise
 		PRINT('makeCompteEntreprise OK');
