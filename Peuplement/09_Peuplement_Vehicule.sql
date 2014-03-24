ea1d07f0b710f70c3404ff3058e687285c8f1f1f
Alexis <Alexis@salleTA>
2014-03-24 14:55:33 +0100

    fix peupleument Vehicules

--- Peuplement/09_Peuplement_Vehicule.sql
+++ Peuplement/09_Peuplement_Vehicule.sql
@@ -25,7 +25,7 @@ INSERT INTO Vehicule (matricule,kilometrage,couleur,statut,num_serie,marque_mode
 		('1775896we','14000','Noir','Disponible','ABS X0988 151','BMW','5 F10 M5',5,'Diesel','false'),
 		('1775896wr','25000','Noir','Disponible','ABS X0988 152','BMW','5 F10 M5',5,'Diesel','false'),
 		('1775896wt','35000','Noir','Disponible','ABS X0988 153','BMW','5 F10 M5',5,'Diesel','false'), 
-		('1775896wy','30000','Noir','Disponible','ABS X0988 154','BMW','5 F10 M5',3,'Essence','false'), 
+		('1775896wy','30000','Noir','Disponible','ABS X0988 154','BMW','5 F10 M5',5,'Essence','false'), 
 		('1775896wu','60000','Noir','Perdue','ABS X0988 155','BMW','5 F10 M5',5,'Essence','false'), 
 		('1775896wi','120000','Noir','Disponible','ABS X0988 156','BMW','5 F10 M5',5,'Essence','false');   	
 GO
