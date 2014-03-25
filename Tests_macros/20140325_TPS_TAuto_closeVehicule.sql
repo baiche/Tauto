0944f3b94442fcdd4a9931f6fbb4e6f195cc03b7
unknown <boris@TIBO.starwars.dess-gla.infop6.jussieu.fr>
2014-03-25 14:58:36 +0100

    correction can extend contrat location.

--- Tests_macros/20140325_TPS_TAuto_closeVehicule.sql
+++ Tests_macros/20140325_TPS_TAuto_closeVehicule.sql
@@ -46,4 +46,61 @@ END CATCH
 GO
 
 --Test 2
--- argument NULL
+-- matricule NULL
+BEGIN TRY
+	DECLARE @ReturnValue int;
+	EXEC @ReturnValue = dbo.closeVehicule
+		@matricule = NULL;
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 2 - Ne drevrais pas etre accepte - KO');
+	END
+	ELSE	
+	BEGIN
+		PRINT('------------------------------Test 2 - OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 2 - Exception leve - KO');
+END CATCH
+GO
+
+--Test 3
+-- matricule inexistant
+BEGIN TRY
+	DECLARE @ReturnValue int;
+	EXEC @ReturnValue = dbo.closeVehicule
+		@matricule = '1435896wy';
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 3 - Ne drevrais pas etre accepte - KO');
+	END
+	ELSE	
+	BEGIN
+		PRINT('------------------------------Test 3 - OK');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 3 - Exception leve - KO');
+END CATCH
+GO
+
+--Test 4
+--vehicule present dans une reservation (a modifier)
+BEGIN TRY
+	DECLARE @ReturnValue int;
+	EXEC @ReturnValue = dbo.closeVehicule
+		@matricule = '0775896we';
+	IF ( @ReturnValue = 1)
+	BEGIN
+		PRINT('------------------------------Test 4 - Ne drevrais pas etre accepte - KO');
+	END
+	ELSE	
+	BEGIN
+		PRINT('------------------------------Test 4 - OK temporairement');
+	END
+END TRY
+BEGIN CATCH
+	PRINT('------------------------------Test 4  - Exception leve - KO');
+END CATCH
+GO
