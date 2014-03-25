7fb7ffc0e7a75543e0fbc2b4c1458d1962a24eee
Lecoconnier David <david.lecoconnier@free.fr>
2014-03-25 01:51:42 +0100

    modifyAbonnement tested

--- Procedures/MacroProcedure_Utilisateur/V2/modifyAbonnement.sql
+++ Procedures/MacroProcedure_Utilisateur/V2/modifyAbonnement.sql
@@ -2,12 +2,11 @@
 -- Fichier     : modifyAbonnement.sql
 -- Date        : 15/03/2014
 -- Version     : 1.0
--- Auteur      : Boris de Finance
+-- Auteur      : David Lecoconnier
 -- Correcteur  : 
 -- Testeur     : 
 -- Integrateur : 
--- Commentaire : Methode modifant les champs d'un compte
--- autre que ceux permettant de l'identifier
+-- Commentaire : Modifie le renouvellement automatique de l'abonnement
 ------------------------------------------------------------
 
 USE TAuto_IBDR;
@@ -23,9 +22,21 @@ AS
 	BEGIN TRANSACTION modifyAbonnement
 	BEGIN TRY
 		
-		COMMIT TRANSACTION modifyAbonnement
-		PRINT('modifyAbonnement OK');
-		RETURN 1;
+		IF @renouvellement_auto IS NOT NULL AND @id IS NOT NULL
+		BEGIN
+			UPDATE Abonnement
+			SET renouvellement_auto = @renouvellement_auto
+			WHERE id = @id;
+			COMMIT TRANSACTION modifyAbonnement
+			PRINT('modifyAbonnement OK');
+			RETURN 1;
+		END
+		ELSE
+		BEGIN
+			PRINT('renouvellement_auto ou id nul');
+			ROLLBACK TRANSACTION modifyAbonnement
+			RETURN -1;
+		END
 	END TRY
 	BEGIN CATCH
 		PRINT('modifyAbonnement: ERROR');
