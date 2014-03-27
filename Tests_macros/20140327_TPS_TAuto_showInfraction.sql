faabce4b1ea958f0e55c51bb47f19e41abbdc6eb
baiche <baiche.mourad89@live.fr>
2014-03-27 21:15:34 +0100

    modification de la fonction fixInfraction.sql et son test

--- Tests_macros/20140327_TPS_TAuto_showInfraction.sql
+++ Tests_macros/20140327_TPS_TAuto_showInfraction.sql
@@ -18,7 +18,7 @@ BEGIN TRY
 	EXEC dbo.makeInfraction '0775896wt','2014-02-16T00:00:00','exces de vitesse',75,'roule a 90 km en plein aglomeration' ;	
 	EXEC dbo.showInfraction '0775896wt','2014-02-16T00:00:00';
 	
-	EXEC dbo.fixInfraction '0775896wt','2014-02-16T00:00:00';
+	EXEC dbo.fixInfraction '0775896wt','Marshall','Michel',75,2,'2014-02-16T00:00:00';
 	EXEC dbo.showInfraction '0775896wt','2014-02-16T00:00:00';
 	
 	PRINT('infraction affiché --- Test OK');
