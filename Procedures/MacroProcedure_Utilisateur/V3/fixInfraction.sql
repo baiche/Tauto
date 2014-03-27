6df30e2a1570a4289b8322476573a7aa1564a368
MohamedNETI <neti.mohamed@gmail.com>
2014-03-27 15:33:06 +0100

    ixInfraction.sql procedure et test OK

--- Procedures/MacroProcedure_Utilisateur/V3/fixInfraction.sql
+++ Procedures/MacroProcedure_Utilisateur/V3/fixInfraction.sql
@@ -17,11 +17,20 @@ GO
 
 CREATE PROCEDURE dbo.fixInfraction
 	@matricule				nvarchar(50), -- FK
-	@nom 					nvarchar(50), -- nullable
-	@montant 				money -- nullable
+	@date					datetime
 AS
 	BEGIN TRANSACTION fixInfraction
 	BEGIN TRY
+	
+		DECLARE @id_location INT;
+		SET @id_location = (SELECT l.id FROM Location l,Infraction i 
+										WHERE l.matricule_vehicule=@matricule 
+										AND  l.id=i.id_location
+										AND i.date = @date);	
+										
+		UPDATE Infraction SET regle='true' WHERE date = @date			
+										   AND id_location = @id_location;			
+
 		COMMIT TRANSACTION fixInfraction
 		PRINT('fixInfraction OK');
 		RETURN 1;
